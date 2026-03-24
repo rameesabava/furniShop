df96bdd657ab422b4746df935788d6a3b3c4768f
rameesabava <rameesabava91@gmail.com>
2026-03-24 23:55:07 +0530

    completed

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://furnishop-server.onrender.com",
     timeout:5000
 })
-
 
 // response interceptors
 axiosInstance.interceptors.response.use(
